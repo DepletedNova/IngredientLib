d32323d6adcfd46c8dd4e44bce26c72d7a7ed9ed
Zoey D <zdavis8529@gmail.com>
2023-05-20 23:06:32 -0600

    Materials workaround

--- Main.cs
+++ Main.cs
@@ -46,84 +46,97 @@ namespace IngredientLib
         private void AddMaterials()
         {
             // Common
-            AddMaterial(CreateTransparent("Glass", 0xF6FEFF, 0.3f));
-            AddMaterial(CreateTransparent("Plastic - Transparent", 0xF6FEFF, 0.7f));
+            AddTransparent("Glass", 0xF6FEFF, 0.3f);
+            AddTransparent("Plastic - Transparent", 0xF6FEFF, 0.7f);
 
-            AddMaterial(CreateFlat("Stem", 0x6B3E26));
-            AddMaterial(CreateFlat("White Fruit", 0xE5FFDC));
+            AddFlat("Stem", 0x6B3E26);
+            AddFlat("White Fruit", 0xE5FFDC);
 
-            AddMaterial(CreateFlat("Emoji Yellow", 0xFAC036));
-            AddMaterial(CreateFlat("Emoji Orange", 0xE48C15));
+            AddFlat("Emoji Yellow", 0xFAC036);
+            AddFlat("Emoji Orange", 0xE48C15);
 
             // Specific
-            AddMaterial(CreateFlat("Vinegar", 0xFFF0C6));
+            AddFlat("Vinegar", 0xFFF0C6);
 
-            AddMaterial(CreateFlat("Chocolate", 0x7B3F00));
-            AddMaterial(CreateFlat("Chocolate Dark", 0x753B00));
-            AddMaterial(CreateFlat("Chocolate Light", 0x854400));
+            AddFlat("Chocolate", 0x7B3F00);
+            AddFlat("Chocolate Dark", 0x753B00);
+            AddFlat("Chocolate Light", 0x854400);
 
-            AddMaterial(CreateFlat("Butter", 0xFAF28D));
+            AddFlat("Butter", 0xFAF28D);
 
-            AddMaterial(CreateFlat("Banana", 0xFFFBC9));
-            AddMaterial(CreateFlat("Banana Peel", 0xFFE135));
-            AddMaterial(CreateFlat("Banana Inner", 0xE5DE6E));
+            AddFlat("Banana", 0xFFFBC9);
+            AddFlat("Banana Peel", 0xFFE135);
+            AddFlat("Banana Inner", 0xE5DE6E);
 
-            AddMaterial(CreateFlat("Lemon", 0xFFF443));
-            AddMaterial(CreateFlat("Lemon Inner", 0xCCBB34));
-            AddMaterial(CreateFlat("Lemon Juice", 0xFFF26D));
+            AddFlat("Lemon", 0xFFF44);
+            AddFlat("Lemon Inner", 0xCCBB34);
+            AddFlat("Lemon Juice", 0xFFF26D);
 
-            AddMaterial(CreateFlat("Lime", 0x549231));
-            AddMaterial(CreateFlat("Lime Inner", 0x497109));
-            AddMaterial(CreateFlat("Lime Juice", 0xBCE896));
+            AddFlat("Lime", 0x549231);
+            AddFlat("Lime Inner", 0x497109);
+            AddFlat("Lime Juice", 0xBCE896);
 
-            AddMaterial(CreateFlat("Oat Mound", 0xdfc496));
-            AddMaterial(CreateFlat("Oat Grain", 0xd8c29d));
+            AddFlat("Oat Mound", 0xdfc496);
+            AddFlat("Oat Grain", 0xd8c29d);
 
-            AddMaterial(CreateFlat("Pork Fat", 0xFFCBCB));
-            AddMaterial(CreateFlat("Pork", 0xE291AC));
-            AddMaterial(CreateFlat("Porkchop Fat", 0xB38F5A));
-            AddMaterial(CreateFlat("Porkchop", 0xEADCB3));
-            AddMaterial(CreateFlat("Bacon Fat", 0xC5A099));
-            AddMaterial(CreateFlat("Bacon", 0x8E3D2F));
+            AddFlat("Pork Fat", 0xFFCBCB);
+            AddFlat("Pork", 0xE291AC);
+            AddFlat("Porkchop Fat", 0xB38F5A);
+            AddFlat("Porkchop", 0xEADCB3);
+            AddFlat("Bacon Fat", 0xC5A099);
+            AddFlat("Bacon", 0x8E3D2F);
 
-            AddMaterial(CreateFlat("Raw Chicken", 0xFFBEBE));
-            AddMaterial(CreateFlat("Cooked Chicken", 0xB95B24));
+            AddFlat("Raw Chicken", 0xFFBEBE);
+            AddFlat("Cooked Chicken", 0xB95B24);
 
-            AddMaterial(CreateFlat("Cooked Drumstick", 0xB65B28));
-            AddMaterial(CreateFlat("Raw Drumstick Bone", 0xC9B9B4));
-            AddMaterial(CreateFlat("Cooked Drumstick Bone", 0xC69067));
+            AddFlat("Cooked Drumstick", 0xB65B28);
+            AddFlat("Raw Drumstick Bone", 0xC9B9B4);
+            AddFlat("Cooked Drumstick Bone", 0xC69067);
 
-            AddMaterial(CreateFlat("Honey", 0xEBA937));
+            AddFlat("Honey", 0xEBA937);
 
-            AddMaterial(CreateFlat("Egg Dough", 0xF4E2AA));
+            AddFlat("Egg Dough", 0xF4E2AA);
 
-            AddMaterial(CreateFlat("Caramel", 0xC68E17));
+            AddFlat("Caramel", 0xC68E17);
 
-            AddMaterial(CreateFlat("Cinnamon", 0xC58C66));
+            AddFlat("Cinnamon", 0xC58C66);
 
-            AddMaterial(CreateFlat("Garlic", 0xf2e9d2));
+            AddFlat("Garlic", 0xf2e9d2);
 
-            AddMaterial(CreateFlat("Jalapeno", 0x548042));
+            AddFlat("Jalapeno", 0x548042);
 
-            AddMaterial(CreateFlat("Blueberry", 0x4f86f7));
-            AddMaterial(CreateFlat("Blueberry 2", 0x115bf4));
+            AddFlat("Blueberry", 0x4f86f7);
+            AddFlat("Blueberry 2", 0x115bf4);
 
-            AddMaterial(CreateFlat("Tortilla", 0xE4DABF));
-            AddMaterial(CreateFlat("Tortilla Spots", 0xD59D62));
-            AddMaterial(CreateFlat("Toasted Tortilla", 0xBF9C70));
-            AddMaterial(CreateFlat("Toasted Tortilla Spots", 0xAC834E));
+            AddFlat("Tortilla", 0xE4DABF);
+            AddFlat("Tortilla Spots", 0xD59D62);
+            AddFlat("Toasted Tortilla", 0xBF9C70);
+            AddFlat("Toasted Tortilla Spots", 0xAC834E);
 
-            AddMaterial(CreateFlat("Spinach", 0x4D7E33));
-            AddMaterial(CreateFlat("Spinach Stem", 0x7C9C4F));
+            AddFlat("Spinach", 0x4D7E33);
+            AddFlat("Spinach Stem", 0x7C9C4F);
 
-            AddMaterial(CreateFlat("Avocado", 0x4E7030));
-            AddMaterial(CreateFlat("Avocado Inside", 0xBCC067));
-            AddMaterial(CreateFlat("Avocado Mash", 0x89AA40));
+            AddFlat("Avocado", 0x4E7030);
+            AddFlat("Avocado Inside", 0xBCC067);
+            AddFlat("Avocado Mash", 0x89AA40);
 
-            AddMaterial(CreateFlat("Strawberry", 0xCC2E36));
-            AddMaterial(CreateFlat("Strawberry Inside", 0xF7C3A8));
+            AddFlat("Strawberry", 0xCC2E36);
+            AddFlat("Strawberry Inside", 0xF7C3A8);
 
             Log("Loaded materials.");
+        }
+
+        // IMPORTANT - I do not recommend the "IngredientLib - " format in your mods. I want to remove it eventually.
+        private void AddFlat(string name, int color)
+        {
+            AddMaterial(CreateFlat(name, color));
+            AddMaterial(CreateFlat($"IngredientLib - \"{name}\"", color));
+        }
+
+        private void AddTransparent(string name, int color, float transparency)
+        {
+            AddMaterial(CreateTransparent(name, color, transparency));
+            AddMaterial(CreateTransparent($"IngredientLib - \"{name}\"", color, transparency));
         }
 
         protected override void OnPostActivate(Mod mod)
